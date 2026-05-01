c1dd84d89e1c9e01069dcd22172ac855d04c1664
Anagha-Raveendrann <131240777+Anagha-Raveendrann@users.noreply.github.com>
2026-05-01 21:34:04 +0530

    login screen completed

--- lib/features/appoinments/presentation/screens/login_screen.dart
+++ lib/features/appoinments/presentation/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:appoinments_booking/features/appoinments/presentation/widgets/app_textfield.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -23,7 +24,9 @@ class LoginScreen extends StatelessWidget {
                   fit: BoxFit.cover,
                   ),
                 ),
+
                 // Blur effect 
+
                 BackdropFilter(
                   filter: ImageFilter.blur(
                     sigmaX: 8,
@@ -54,8 +57,85 @@ class LoginScreen extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 30,
                     fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                        ),
                   ),
-                  )
+                  const SizedBox(height: 30,),
+
+                  // Email label
+
+                  const Text(
+                    "Email",
+                   style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 8,),
+
+                  // Reusable Email Field
+                  AppTextfield(
+                    hintText: "Enter your email",
+                     controller: emailController,
+                       KeyboardType: TextInputType.emailAddress,
+                       prefixIcon: Icons.email,
+                       ),
+                    const SizedBox(height: 20,
+                    ),
+
+                    // password label
+
+                    Text("Password",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                    ),
+                  // Reusable password field
+                  AppTextfield(
+                    hintText: "Enter password", 
+                    controller: passordController,
+                    isPassword: true,
+                    prefixIcon: Icons.lock,
+                    ),
+                    const SizedBox(height: 40,),
+
+                    // login button 
+                    SizedBox(
+                      width: double.infinity,
+                      height: 55,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xff0E6B3E),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(14)
+                          )
+                        ),
+                        onPressed: (){}, 
+                        child: Text("Login",
+                        style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 25,),
+                     
+                    // terms & privacy text
+                    const Center(
+                      child: Text.rich(
+                        TextSpan(
+                          text: "By creating or logging into an account you are agreeing\nwith our ",
+                          style: TextStyle(color: Colors.black45),
+                          children: [
+                            TextSpan(
+                              text: "Terms and Conditions",
+                              style: TextStyle(color: Colors.blue)
+                            ),
+                            TextSpan(text: "and",
+                            style: TextStyle(color: Colors.blue),
+                            ),
+                            TextSpan(
+                              text: "Privacy Policy",
+                              style: TextStyle(color: Colors.blue)
+                            ),
+                          ]
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
                 ],
               ),
             ),
---
+++ lib/features/appoinments/presentation/widgets/app_textfield.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class AppTextfield extends StatelessWidget {
+   
+    final String hintText;
+    final TextEditingController controller;
+    final bool isPassword;
+    final TextInputType KeyboardType;
+    final IconData? prefixIcon;
+
+   AppTextfield({
+    super.key, 
+    required this.hintText,
+     required this.controller, 
+     this.isPassword = false,
+      this.KeyboardType = TextInputType.text,
+       this.prefixIcon});
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return TextField(
+      controller: controller ,
+      obscureText: isPassword,
+      keyboardType:KeyboardType,
+      style: const TextStyle(fontSize: 16),
+      decoration: InputDecoration(
+      hintText: hintText,
+
+        // Background color
+        filled: true,
+        fillColor: Colors.white,
+
+        // prefix icon 
+        prefixIcon : prefixIcon != null ? Icon(prefixIcon, color: Colors.grey,) : null,
+        
+         // padding 
+         contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+
+          // Border style
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(14),
+            borderSide: BorderSide.none,
+          ),
+
+          // focus border 
+          focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: const BorderSide(color: Color(0xff0E6B3E), )
+          ),
+      ),
+    );
+  }
+}
